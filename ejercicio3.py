31e70df4e73a9c8f4e11c1d50bf5b4c4860fef0d
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-05-06 12:06:52 -0600

    Ejercicios 06/05/24

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+print("Dame el valor de la base: ")
+x = float(input())
+
+print("Dame el valor de la altura: ")
+y = float(input())
+
+print("El area es: ")
+print(x * y)
---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+import math 
+
+print("Dame el diametro del circulo")
+
+diametro = float(input())
+radio = diametro / 2
+area = math.pi * radio * radio
+
+print("El area es: ")
+print(area)
